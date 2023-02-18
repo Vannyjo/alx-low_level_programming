b11bbb103d84dd08c0dda5f7fe2ef539226bc7b5
Vannyjo <ezejoseph96@gmail.com>
2023-02-18 15:35:24 -0800

    yask 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program thhat print a line uding the printf functin
+ * Return : 0 (Success)
+ */
+
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
