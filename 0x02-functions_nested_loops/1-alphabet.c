cbc4662f8ae82c296f98dced79ff6bcfd21d2e94
Vannyjo <ezejoseph96@gmail.com>
2023-02-22 00:40:09 -0800

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,13 @@
 #include "main.h"
 /**
  * print_alphabet - print alphabet in lower case
- * Return: Always 0 (success)
+ * Return:void
  */
 void print_alphabet(void)
 {
 	char eze;
 
-	for (eze = 'a'; eze = 'z'; eze++)
-	{
-	_putchar(eze);
-	}
-	_putchar('\n');
-	return (0);
+	for (eze = 'a'; eze <= 'z'; eze++)
+		_putchar(eze);
+			_putchar('\n');
 }
