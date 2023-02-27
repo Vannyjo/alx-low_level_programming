840fa38108749f0f401de41c5441ea0f4c4eef8f
Vannyjo <ezejoseph96@gmail.com>
2023-02-27 14:45:37 -0800

    main

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,8 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-void swap_int(int *a, int *b);
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 nt _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
