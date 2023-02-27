497035cd65be858784d923ccae80710a0501045b
Vannyjo <ezejoseph96@gmail.com>
2023-02-27 14:41:33 -0800

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,8 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * void reset_to_98 - change the pointers direction
- * @: input
- * Return: n
+ * @n: input
+ * Return: 0
  */
 
 void reset_to_98(int *n);
