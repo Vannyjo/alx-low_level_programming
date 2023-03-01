8d585c5d1546612d31f93cb1bbd7d7b704bf9378
Vannyjo <ezejoseph96@gmail.com>
2023-02-28 16:29:24 -0800

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * puts2 - entry point
+ * @str: other characters of a string
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+
+	int 0;
+
+
+	while (*y != '\0')
+
+	{
+		y++;
+		longi++;
+	}
+	t = longi - 1;
+	for (o = 0 ; o <= t ; o++)
+	{
+		if (o % 2 == 0)
+		{
+			_putchar(str[o]);
+		}
+	}
+	_putchar('\n');
+}
