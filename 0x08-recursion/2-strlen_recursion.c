43e283983493780b03ef4b401fd9ba78550e70f3
Vannyjo <ezejoseph96@gmail.com>
2023-03-09 14:21:50 -0800

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Entry point
+ * @s: Returns the length of the string
+ * Return: 0
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		int _strlen_recursion(char *s);
+		 _putchar(*s);
+	}
+	return ('\0');
+}
