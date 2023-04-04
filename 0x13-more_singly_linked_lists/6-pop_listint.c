55809cd4fe9d74832f8ca82d6bd695a38e9ddcee
Vannyjo <ezejoseph96@gmail.com>
2023-04-04 09:54:53 -0700

    this code deletes the head node

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,14 +9,14 @@
  */
 int pop_listint(listint_t **head)
 {
-	listint_t *startNode;
+	listint_t *start;
 	int n;
 
 	if (head == NULL || *head == NULL)
 		return (0);
-	startNode = *head;
-	*head = startNode->next;
-	n = startNode->n;
-	free(startNode);
+	start = *head;
+	*head = start->next;
+	n = start->n;
+	free(start);
 	return (n);
 }
