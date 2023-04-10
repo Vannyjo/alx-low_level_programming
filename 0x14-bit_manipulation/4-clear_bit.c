34a853510ff5185044ac1f7cc198cdd120fc554b
Vannyjo <ezejoseph96@gmail.com>
2023-04-10 09:12:01 -0700

    hot fix

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,13 +8,13 @@
  * Return: 1 if successful, or -1 if an error occurred
  */
 int clear_bit(unsigned long int *n, unsigned int index)
-
 {
-    if (index >= sizeof(unsigned long int) * 8 /* check if index is too large */
+	unsigned long int bit;
 
-        return (-1);
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+	bit = ~(1 << index);
+	*n = *n & bit;
 
-    *n &= ~(1 << index); /* clear bit at index by ANDing with a mask with 0 at index position */
-
-    return (1); /* return 1 to indicate success */
+	return (1);
 }
