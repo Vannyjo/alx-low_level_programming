326b85f34e94b6cc4e10058d005064b8b40b697f
Vannyjo <ezejoseph96@gmail.com>
2023-04-10 13:08:53 -0700

    hot fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include "main.h"
+
 /**
  * create_file -creates an array of chars, and initializes
  *
@@ -7,7 +15,6 @@
  *
  * Return: 1 on success, -1 on failure
  */
-
 
 int create_file(const char *filename, char *text_content) {
     if (filename == NULL) {
