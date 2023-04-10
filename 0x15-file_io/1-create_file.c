1dfe360063f1352091b1e7810acd82ec1f261fc4
Vannyjo <ezejoseph96@gmail.com>
2023-04-10 13:12:17 -0700

    hot fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,12 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include "main.h"
-
 /**
  * create_file -creates an array of chars, and initializes
  *
@@ -15,23 +7,26 @@
  *
  * Return: 1 on success, -1 on failure
  */
+int create_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
 
-int create_file(const char *filename, char *text_content) {
-    if (filename == NULL) {
-        return -1;
-    }
-    int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (fd == -1) {
-        return -1;
-    }
-    if (text_content != NULL) {
-        ssize_t len = strlen(text_content);
-        ssize_t written = write(fd, text_content, len);
-        if (written != len) {
-            close(fd);
-            return -1;
-        }
-    }
-    close(fd);
-    return 1;
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
 }
