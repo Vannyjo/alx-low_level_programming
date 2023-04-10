17d4170e6e12e452abc6a9bcf6d26792a7c59483
Vannyjo <ezejoseph96@gmail.com>
2023-04-10 09:22:56 -0700

    hot fix

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -7,12 +7,11 @@
  */
 int get_endianness(void)
 {
-	unsigned int i = 1;
-	char *c = (char *) &i;
+	unsigned int machine = 1;
+	char *check = (char *) &machine;
 
-	if (*c)
+	if (*check)
 		return (1); /* little endian */
 	else
 		return (0); /* big endian */
 }
-
